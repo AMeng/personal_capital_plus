b8d2e92a93e2f6a90d285146c583c3ae4d5106d5
AMeng <alex.meng@segment.com>
2024-04-21 13:57:10 -0600

    remove background.js. no longer needed for manifest v3

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-  chrome.scripting.executeScript({
-    target: {tabId: details.tabId},
-    files: ["main.js"]
-  });
-});
